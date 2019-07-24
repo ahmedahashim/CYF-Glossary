cc7ab9e768098d3071672d6c01f17676a627fe2e
mtbdata711 <bengethomasmatthew@gmail.com>
2019-07-24 22:48:34 +0100

    adjust single search result styling to remove bugs

--- src/containers/SingleSearchResult.js
+++ src/containers/SingleSearchResult.js
@@ -1,24 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./SingleSearchResult.css";
+import "./Grid.css";
 
 export default class SingleSearchResult extends Component {
   render() {
     return (
-      <div className="col-sm-12 single-result" key={this.props.key}>
-        <Link className='single-result-link' to={`/${this.props.topicSlug}/${this.props.termSlug}`} >
+      <div className="sm-col-12 single-result">
+        <Link
+          className="single-result-link"
+          to={`/${this.props.topicSlug}/${this.props.termSlug}`}
+        >
           {this.props.term}
         </Link>
         <div className="content">
           <p>{this.props.definition}</p>
         </div>
         <Link to={`/search/${this.props.topic}`}>
-        <span className="badge result-badge-topic result-badge-margin">
-        {this.props.topic}
-        </span>
+          <span className="badge result-badge-topic result-badge-margin">
+            {this.props.topic}
+          </span>
         </Link>
-          
-       
       </div>
     );
   }
